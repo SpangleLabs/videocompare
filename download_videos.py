08eb2a9727d582be42cbe240d6fc618aaf831f05
joshcoales <joshua@coales.co.uk>
2019-09-10 23:26:43 +0100

    Fixing up downloader

--- download_videos.py
+++ download_videos.py
@@ -1,16 +1,16 @@
-from telethon import TelegramClient
-from tqdm import tqdm
+from telethon.sync import TelegramClient
 
 
 # These example values won't work. You must get your own api_id and
 # api_hash from `my.telegram.org` , under API Development.
 api_id = APIID
 api_hash = 'APIHASH'
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient('deer_gifs_downloader', api_id, api_hash)
 client.start()
-print(client.get_me().stringify())
+# print(client.get_me())
 
-messages = client.get_messages('deergifs', limit=2000)
-print(len(messages))
-for msg in tqdm(messages):
-    client.download_media(msg)
+channel_username = 'deergifs'
+channel_entity = client.get_entity(channel_username)
+for message in client.iter_messages(channel_entity):
+    print("Downloading message: {}".format(message))
+    client.download_media(message)
